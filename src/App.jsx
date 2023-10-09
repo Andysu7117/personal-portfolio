0415400acabefb23c730ea0cc2b40cedafdcee2b
Andy <andy.su7117@gmail.com>
2023-10-09 17:53:33 +1100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import AboutMe from "./Components/AboutMe";
+import Contact from "./Components/Contact";
+import Navbar from "./Components/Navbar";
+import Projects from "./Components/Projects";
+import Resume from "./Components/Resume";
+
+export default function App() {
+  return (
+    <main className="text-gray-400 bg-gray-900 body-font">
+      <Navbar />
+      <AboutMe />
+      <Projects />
+      <Contact />
+      <Resume />
+    </main>
+  );
+}
