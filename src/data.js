ae3ca9c3824df0b691a5064d291d85889a852b16
Andy <andy.su7117@gmail.com>
2023-10-09 21:43:29 +1100

    final touchups

--- src/data.js
+++ src/data.js
@@ -2,33 +2,47 @@ export const projects = [
   {
     title: "Travel Gem Exlorer",
     subtitle: "Fullstack",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-    image: "",
+    description: `Website for finding and posting hidden and not well known travel locations. Highlights of this project are:
+      User Authentication.
+      Developing RESTFUL API’s
+      Full-stack application
+      Wide range of npm packages used
+      `,
+    image: "./travel-gem.png",
     link: "https://github.com/csetiawan88/travel-gem-explorer",
   },
   {
     title: "Employee Database",
     subtitle: "SQL",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-    image: "",
+    description: `Worked with SQL, JavaScript and Node.js to create a CLI application where you can:
+      - View Employees, Roles, and Departments
+      - Add Employees, Roles and Departments
+      - Update an Employee's role
+      - Delete an employee`,
+    image: "./employee-database.png",
     link: "https://github.com/Andysu7117/employee-database",
   },
   {
     title: "The Blog",
     subtitle: "Handlebars.js",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-    image: "",
+    description: `A blog website where users can post and comment on posts. This application showcases:
+      - Login and signup functionalities
+      - Creating, deleting and updating posts and comments
+      - RESTFUL API’s
+      - Collaborative, team project
+      `,
+    image: "./TheBlog.png",
     link: "https://github.com/Andysu7117/TheBlog",
   },
   {
     title: "Social Media Backend",
     subtitle: "MongoDB and Mongoose",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-    image: "",
+    description: `Utilised MongoDB and Mongoose to create a backend where a user can:
+      - Create Account/User
+      - Add Thoughts
+      - Add friends
+      `,
+    image: "./social-media.png",
     link: "https://github.com/Andysu7117/social-media-network",
   },
 ];
