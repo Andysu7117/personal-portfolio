c23933fafb8afb7f6f60fe900c8ad721a22cf0ac
Andy <andy.su7117@gmail.com>
2023-10-09 23:35:23 +1100

    changed to tailwindcss v3

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,7 @@
 module.exports = {
-  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./index.html"],
-  darkMode: false, // or 'media' or 'class'
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./index.html"],
+  darkMode: "media", // or 'class'
   theme: {
-    extend: {},
-  },
-  variants: {
     extend: {},
   },
   plugins: [],
